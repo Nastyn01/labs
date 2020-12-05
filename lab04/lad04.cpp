12a047f09602e675ff725a1608986401d5751970
Nastyn01 <sizonenko0110@gmail.com>
2020-12-05 19:20:49 +0200

    changed lab04

--- lab04/lad04.cpp
+++ lab04/lad04.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+void y1(int x);
+void y2(int x);
+
 int main() {
     for (int x = -5; x <= 2; x++) {
+        y1(x);
+        y2(x);
+    }
+    void y1(int x) {
         double y1 = (double)-1 / x;
         printf("y1 = -1 / x: x = %i, y = %f \n", x, y1);
     }
-    for (int x = -5; x <= 2; x++) {
+    void y2(int x) {
         int y2 = x ^ 2;
         printf("y2 = x^2: x = %i, y = %i \n", x, y2);
     }
